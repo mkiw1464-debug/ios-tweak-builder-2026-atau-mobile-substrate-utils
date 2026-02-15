5d7566d14acdc7634438a3504e2829fdae22f82d
mkiw1464-debug <mkiw1464@gmail.com>
2026-02-15 22:24:50 +0800

    Update Tweak.mm

--- Tweak.mm
+++ Tweak.mm
@@ -1,18 +1,16 @@
-// Tweak.mm - Azurite External (minimal test version)
 #include <substrate.h>
 #include <dlfcn.h>
-#include <sys/ptrace.h>
 
 %ctor {
     @autoreleasepool {
         ptrace(PT_DENY_ATTACH, 0, 0, 0);
 
-        void* il2cpp = dlopen("libil2cpp.so", RTLD_LAZY);
-        if (il2cpp) {
-            // Test hook simple - kalau compile OK, tambah logic nanti
-            void* sym = dlsym(il2cpp, "_ZN12PlayerMovement6UpdateEv");
+        void* handle = dlopen("libil2cpp.so", RTLD_LAZY);
+        if (handle) {
+            // Test hook simple
+            void* sym = dlsym(handle, "_ZN12PlayerMovement6UpdateEv");
             if (sym) {
-                // Hook placeholder
+                // Placeholder hook
             }
         }
     }
